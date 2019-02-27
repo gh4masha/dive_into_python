4401069803dbf567159c8001a16ed994b84b8d33
Maryia Litouchanka <Maryia_Litouchanka@epam.com>
2019-02-27 15:18:14 +0300

    week3 task2

--- week3/week03_02.py
+++ week3/week03_02.py
@@ -1,4 +1,5 @@
 import os.path
+import csv
 
 
 class CarBase:
@@ -13,36 +14,69 @@ class CarBase:
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
-        os.path.splitext(self.photo_file_name)
-        pass
 
 
 class Car(CarBase):
+    passenger_seats_count = int(0)
+
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         self.car_type = "car"
-        pass
+        self.passenger_seats_count = passenger_seats_count
+        super().__init__(brand, photo_file_name, carrying)
 
 
 class Truck(CarBase):
     body_length = float(0)
     body_width = float(0)
     body_height = float(0)
+    body_whl = float(0)
 
     def get_body_volume(self):
-        return self.body_length*self.body_height*self.body_width
+        return self.body_length * self.body_height * self.body_width
 
     def __init__(self, brand, photo_file_name, carrying, body_whl):
+        super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'truck'
-        pass
+        self.body_whl = body_whl or None
 
 
 class SpecMachine(CarBase):
+    extra = ''
+
     def __init__(self, brand, photo_file_name, carrying, extra):
         self.car_type = 'spec_machine'
-        pass
-
+        self.extra = extra
+        super().__init__(brand, photo_file_name, carrying)
 
 
 def get_car_list(csv_filename):
     car_list = []
+
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            print(row[0])
+            try:
+                if row[0] == 'car':
+                    if row[1] is not None and row[3] is not None and row[5] is not None and row[2] is not None:
+                        car = Car(row[1], row[3], row[5], row[2])
+                        car_list.append(car)
+                elif row[0] == 'truck':
+                    if row[1] is not None and row[3] is not None and row[5] is not None :
+                        truck = Truck(row[1], row[3], row[5], row[4] or None)
+                        car_list.append(truck)
+                elif row[0] == 'spec_machine':
+                    if row[1] is not None and row[3] is not None and row[5] is not None and row[6] is not None:
+                        spec_machine = SpecMachine(row[1], row[3], row[5], row[6] or None)
+                        car_list.append(spec_machine)
+            except LookupError:
+                pass
+
+    #     0       1             2                   3               4         5        6
+    #  car_type; brand; passenger_seats_count; photo_file_name; body_whl;  carrying; extra
+
     return car_list
+
+
+get_car_list('D:\learn\python\dive_into_python\week3\coursera_week3_cars.csv')
