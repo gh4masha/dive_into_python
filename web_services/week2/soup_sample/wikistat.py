42513d02b18eada75e50cfc427d561d63147731c
m4masha@gmail.com <giPA-haPA5a>
2019-08-14 23:09:59 +0300

    web services; w2: Beautifulsoup tasks 1,2,3

---
+++ web_services/week2/soup_sample/wikistat.py
@@ -0,0 +1,149 @@
+from collections import deque
+
+from bs4 import BeautifulSoup
+import re
+import os
+
+
+# Вспомогательная функция, её наличие не обязательно и не будет проверяться
+def build_tree(start, end, path):
+    # link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
+    files = list(os.listdir(path))  # Словарь вида {"filename1": None, "filename2": None, ...}
+
+    result = dict()
+
+    # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
+    for file in files:
+        all_links = find_all_links_in_file(file, path)
+        result.update({file: []})
+        for link in all_links:
+            filename = str(link)[str(link).rindex('/') + 1:]
+            if link.find(':') == -1 and link.find('#') == -1 \
+                    and filename in files \
+                    and not (filename == file):
+                items = result.get(file)
+                if filename not in items:
+                    items.append(filename)
+                result.update({file: items})
+
+    # print(result)
+    return result
+
+
+# Вспомогательная функция, её наличие не обязательно и не будет проверяться
+def build_bridge(start, end, path):
+    global files
+
+    parents = dict()
+    parents.update({start: None})
+    search_queue = deque()
+    search_queue += [start]
+    searched = []
+    while len(search_queue) > 0:
+        filename = search_queue.popleft()
+
+        for child in files[filename]:
+            if not child in searched:
+                parents.update({child: filename})
+                if child == end:
+                    search_queue.clear()
+                    # print(' len(search_queue) ', len(search_queue))
+                    break
+                else:
+                    searched.append(child)
+                    search_queue += [child]
+
+    # print('the route is found ')
+    # print(parents)
+
+    bridge = deque()
+    bridge.appendleft(end)
+
+    parent = parents.get(end)
+    i = 0
+    while not (parent == start) and (parent is not None) and i < 503:
+        bridge.appendleft(parent)
+        parent = parents.get(parent)
+        # print('next parent: ', parent, ' bridge: ', bridge)
+        i += 1
+
+    bridge.appendleft(start)
+
+    # print('bridge: ', bridge)
+    return bridge
+
+
+def find_all_links_in_file(filename, path):
+    result = []
+    with open("{}{}".format(path, filename)) as data:
+        soup = BeautifulSoup(data, "lxml")
+        for t in soup.find_all('a'):
+            if t.has_attr('href') and str(t['href']).startswith(path[1:]):
+                result.append(t['href'])
+    return result
+
+
+files = {}
+
+
+def parse(start, end, path):
+    """
+    Если не получается найти список страниц bridge, через ссылки на которых можно добраться от start до end, то,
+    по крайней мере, известны сами start и end, и можно распарсить хотя бы их: bridge = [end, start]. Оценка за тест,
+    в этом случае, будет сильно снижена, но на минимальный проходной балл наберется, и тест будет пройден.
+    Чтобы получить максимальный балл, придется искать все страницы. Удачи!
+    """
+
+    global files
+
+    files = build_tree(start, end, path)
+    min_bridge = build_bridge(start, end,
+                              path)  # Искать список страниц можно как угодно, даже так: bridge = [end, start]
+
+    # Когда есть список страниц, из них нужно вытащить данные и вернуть их
+    out = {}
+    for file in min_bridge:
+        with open("{}{}".format(path, file)) as data:
+            soup = BeautifulSoup(data, "lxml")
+
+            body = soup.find(id="bodyContent")
+
+            # TODO посчитать реальные значения
+            # Количество картинок (img) с шириной (width) не меньше 200
+            imgs = 0
+            for t in soup.find_all('img'):
+                if t.has_attr('width') and int(t['width']) >= 200:
+                    imgs += 1
+
+            headers = 0  # Количество заголовков, первая буква текста внутри которого: E, T или C
+            for t in soup.find_all(re.compile('^h[1-6]$')):
+                text = t.getText()
+                if text[0] in ['E', 'T', 'C']:
+                    headers += 1
+
+            max_linkslen = 0  # Длина максимальной последовательности ссылок, между которыми нет других тегов
+            t = soup.find('a')
+            while not (t == None):
+                linkslen = 0  # Длина максимальной последовательности ссылок, между которыми нет других тегов
+                next_subling = t.find_next_sibling()
+                while not (next_subling == None) and next_subling.name == 'a':
+                    linkslen += 1
+                    # print('next_sibling>>>> ', next_subling)
+                    next_subling = next_subling.find_next_sibling()
+
+                if max_linkslen < linkslen:
+                    max_linkslen = linkslen
+
+                if next_subling == None:
+                    t = t.find_next('a')
+                else:
+                    t = next_subling.find_next('a')
+
+            lists = 0  # Количество списков, не вложенных в другие списки
+            for t in soup.find_all(re.compile('^[uo]l$')):
+                if len(t.findParents(re.compile('^[uo]l$'))) == 0:
+                    lists += 1
+
+            out[file] = [imgs, headers-1, max_linkslen+1, lists]
+
+    return out
